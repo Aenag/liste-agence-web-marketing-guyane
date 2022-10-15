56e48de6c31f16e53b67e1b4a725c4b5a5a70882
Webmonster Antilles <110256555+WebmonsterA@users.noreply.github.com>
2022-10-15 11:39:08 -0400

    Version 1.0.0

---
+++ agences-guyane.sql
@@ -0,0 +1,39 @@
+CREATE TABLE agences-guyane(
+   NOM     VARCHAR(40) NOT NULL PRIMARY KEY
+  ,ADDRESS VARCHAR(29)
+  ,ADRESS2 VARCHAR(27)
+  ,CPO     INTEGER 
+  ,CITY    VARCHAR(23)
+  ,PHONE1  VARCHAR(14)
+  ,PHONE2  VARCHAR(13)
+  ,EMAIL   VARCHAR(30)
+  ,URL     VARCHAR(46)
+  ,TYPE    VARCHAR(9) NOT NULL
+);
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Agence BA marketing','9 lot Anse du Mahury','Impasse Avecinnia',97354,'Remire-Montjoly','0694 92 29 20',NULL,'beatrice.nollet@bamarketing.fr','https://bamarketing.fr/','Marketing');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Agence Web Guyane','494 route Montabo',NULL,97300,'Cayenne','0694 20 09 11',NULL,NULL,NULL,'Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Agence Web Kbatis','Avenue Général de Gaulle',NULL,97300,'Cayenne','0768 42 32 00',NULL,NULL,NULL,'Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Alaisenet','3 bis route Montabo','Lotissement De Ho Kong Ciat',97300,'Cayenne','0783 12 29 67',NULL,'alaisenet@gmail.com','https://alaisenet.fr/','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Bitwip','1 place Schoelcher',NULL,97300,'Cayenne','0980 80 82 42',NULL,NULL,'https://bitwip.fr/','Marketing');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('C2i Guyane SARL','13 r Rouget de Lisle',NULL,97300,'Cayenne','0594 38 53 85',NULL,'contact@digitom.fr','https://www.c2i-outremer.com/','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Comimpex','25 rue Jean-Elie Euloge',NULL,97354,'Remire-Montjoly','0594 29 46 32',NULL,NULL,NULL,'Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Côté cube','1 Rue Rouget de l''Isle',NULL,97300,'Cayenne','0594 10 82 20',NULL,'info@cote-cube.fr','https://www.cote-cube.fr/','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Couleurwest-it','3910 avenue Christophe Colomb',NULL,97320,'Saint-Laurent-du-Maroni','0694 94 98 42',NULL,'contact@couleurwest-it.fr','https://couleurwest-it.com/','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('DEV.COM','8 rue Breguet','Z.I. Pariacabo',97310,'Kourou','0594 32 01 56',NULL,NULL,'https://www.devcom-guyane.fr/','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('E-com','2 rue Louis Blanc',NULL,97300,'Cayenne','0694 21 90 94',NULL,'ecomcguyane@gmail.com','https://ecom973.com/','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Galwebdesign','51 rue René Jadfard',NULL,97300,'Cayenne','0694 91 91 85',NULL,NULL,'http://galwebdesign.net','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('IMT Concept','7 impassable d''Abreu',NULL,97300,'Cayenne','0630 57 30 12',NULL,NULL,NULL,'Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Isona','721 Route départemental N5',NULL,97356,'Montsinéry Tonnegrande','0690 30 80 80',NULL,NULL,NULL,'Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Kasim Conseil','17 avenue Mozart',NULL,97310,'Kourou','0694 91 73 37',NULL,'kasim_conseil@presidency.com','https://www.kasimconseil.fr/','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('L''art communique','34 rue Louis Blanc',NULL,97300,'Cayenne','0782 71 57 55',NULL,'hello@lartcommunique.com','https://lartcommunique.com/','Marketing');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Le Service','182 rue Jules Patient','Cite Zephir Ens',97300,'Cayenne','0594 30 93 20',NULL,NULL,NULL,'Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('N.O.V.I. (Network Of Visual Information)','20 lot Artisanal Galmot',NULL,97300,'Cayenne','0694 49 42 43',NULL,'contact@novi-connected.fr','https://novi-connected.fr/','Marketing');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Netactions','Bat. EUROPA E14','Les Jardins de la Madeleine',97300,'Cayenne','0594 28 62 92',NULL,NULL,'https://netactions.net/','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Nuage Com','1 Rue Rouget de l''Isle',NULL,97300,'Cayenne','0694 91 10 03','0694 27 59 03','contact@nuagecom.com','https://nuagecom.com/','Marketing');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Orphose.studio','14 rue Alphone Bocs',NULL,97351,'Matouri',NULL,NULL,'start@orphose.studio','https://www.orphose.studio/','Marketing');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Priv studio','55 avenue Voltaire',NULL,97300,'Cayenne',NULL,NULL,'hello@priv.studio',NULL,'Marketing');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Publirama Studio','84 Ter rue Victor Schoelcher',NULL,97300,'Cayenne','0594 30 25 82',NULL,NULL,'http://www.publiramastudio.com','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Solocal Outre-mer','Centre Plazza Marengo',NULL,97300,'Cayenne','0590 78 73 80',NULL,NULL,'https://agence.solocal.com/8-solocal-outre-mer','Marketing');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Waltacom','15 rue Eugène Tècle','Cité Thémire',97300,'Cayenne','0694 99 08 29',NULL,'contact@waltacom.fr','https://waltacom.fr/','Marketing');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Web2Web','Rue J. Jauré',NULL,97300,'Cayenne','06 90 47 79 55',NULL,'guyane@web2web.fr','https://web2web.fr/','Web');
+INSERT INTO agences-guyane(NOM,ADDRESS,ADRESS2,CPO,CITY,PHONE1,PHONE2,EMAIL,URL,TYPE) VALUES ('Yanacom','17 rue Vermont Polycarpe',NULL,97300,'Cayenne','0594 29 89 46',NULL,'contact@yanacomguyane.fr','https://yanacomguyane.fr/','Marketing');
